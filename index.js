bb3da20baee3b034da610cfe60fb274ea66e217b
Muiz-PT <abdulmuiz@prioclen.tech>
2025-03-18 20:00:56 +0100

    update: updating code with solution for challenge

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns').promises;
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(cors());
 
@@ -14,10 +18,52 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+
+// solution
+const generateShortUrl = (length = 9) => 
+  Array.from({ length }, () => String.fromCharCode(97 + Math.floor(Math.random() * 26))).join('');
+
+let urlMap = new Map();
+
+app.post("/api/shorturl", async (req, res) => {
+  const url = req.body.url;
+
+  try {
+    const urlObject = new URL(url);
+
+    // only allow http/https urls
+    if (!["http:", "https:"].includes(urlObject.protocol)) {
+      return res.json({ error: 'invalid url' });
+    }
+
+    try {
+      // validate domain
+      await dns.lookup(urlObject.hostname)
+
+      // create new short URL
+      const shortUrl = generateShortUrl();
+      urlMap.set(shortUrl, url);
+
+      return res.json({ original_url: url, short_url: shortUrl });
+    } catch (error) {
+      return res.json({ error: 'invalid url' });
+    }
+  } catch (error) {
+    return res.json({ error: 'invalid url' });
+  }
 });
+
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const shortenedUrl = req.params.short_url;
+
+  if (!urlMap.has(shortenedUrl)) {
+    return res.status(404).json({ error: 'No short URL found' });
+  }
+
+  const originalUrl = urlMap.get(shortenedUrl);
+
+  res.redirect(originalUrl);
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
